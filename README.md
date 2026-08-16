d78b16f7cabe935667cb3ee9e589e038efb47223
JuliaGawlak123 <131684102+JuliaGawlak123@users.noreply.github.com>
2026-08-16 11:51:13 -0300

    Criação da estrutura inicial do projeto

---
+++ README.md
@@ -0,0 +1,13 @@
+# Controle de Projetos
+Sistema desenvolvido em Python para controle e acompanhamento de Projetos.
+
+## Objetivo
+Criar uma aplicação simples para cadastrar, consultar e acompanhar projetos.
+
+## Tecnologias
+- Python
+- Pycharm
+- Git
+- GitHub
+- GitHUb Desktop
+
---
+++ main.py
@@ -0,0 +1,18 @@
+def exibir_menu():
+    print("\n===================================")
+    print("       CONTROLE DE PROJETOS")
+    print("===================================")
+    print("1 - Cadastrar projeto")
+    print("2 - Listar projetos")
+    print("3 - Buscar projeto")
+    print("4 - Alterar status")
+    print("5 - Remover projeto")
+    print("0 - Sair")
+    print("===================================")
+
+def main():
+    print("Sistema iniciado!")
+    exibir_menu()
+
+if __name__ == "__main__":
+    main()
